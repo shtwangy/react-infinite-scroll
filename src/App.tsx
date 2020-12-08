4d73d404973d1e1774c53e807d69d1cbbfa15866
shtwangy <shtwangy@gmail.com>
2020-12-08 21:48:01 +0900

    feat(src/App): add book search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import useBookSearch from "./hooks/useBookSearch";
 
 function App() {
-  return (
-    <div>
-      Hello, Infinite Scroll Trial...
-    </div>
-  );
+    const [query, setQuery] = useState('')
+    const [pageNumber, setPageNumber] = useState(1);
+    const { loading, error, books, hasMore } = useBookSearch(query, pageNumber)
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value)
+        setPageNumber(1)
+    }
+
+    return (
+        <>
+            <h1>Hello, Infinite Scroll Trial...</h1>
+            <input type="text" onChange={handleSearch}></input>
+            {
+                books.length > 0 && (
+                    books.map(book => (
+                        <div key={book}>{book}</div>
+                    ))
+                )
+            }
+            <div>Loading...</div>
+            <div>Error</div>
+        </>
+    );
 }
 
 export default App;
